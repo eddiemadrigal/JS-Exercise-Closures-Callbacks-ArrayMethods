3195156662347371e0c84a11ebce587e4f2bb2d0
Eddie Madrigal <edmadrigal@yahoo.com>
2020-01-28 13:15:11 -0800

    processLastItem() problem complete

--- index.js
+++ index.js
@@ -66,9 +66,10 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function processLastItem(stringList, callback) {
+  const arrLen = stringList.length - 1;
+  return callback(`${stringList[arrLen]}`);
+} 
 
 /**
  * ### Challenge `processSum`

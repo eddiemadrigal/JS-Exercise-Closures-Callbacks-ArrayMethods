a69ac721e3007445e96b44942d13ec3b40db3d09
Eddie Madrigal <edmadrigal@yahoo.com>
2020-01-28 13:51:04 -0800

    processContains() completed.

--- index.js
+++ index.js
@@ -140,8 +140,9 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
+function processContains(item, list, callback) {
+  const found = list.find(element => element === item);
+  return (found ? true : false);
 }
 
 /**
